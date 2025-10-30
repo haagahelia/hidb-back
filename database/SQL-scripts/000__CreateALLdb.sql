1b862dca07a1c6ebc1b3d7c1fb82537d33b645e2
TrangLe265 <trang.le@myy.haaga-helia.fi>
2025-10-30 10:26:12 +0200

    added test data

--- database/SQL-scripts/000__CreateALLdb.sql
+++ database/SQL-scripts/000__CreateALLdb.sql
@@ -39,3 +39,25 @@ CREATE TABLE IF NOT EXISTS Organization (
 /* -------------------------- END --------------------------- */
 /* ---------------------------------------------------------- */
 /* ---------------------------------------------------------- */
+
+USE casedb; 
+
+/* INSERTS */
+/* --- Insert: Aircraft --- */
+INSERT INTO Aircraft (name, manufacturer, model, year_built, weight, aircraft_type, museum_location_number, display_section, description) VALUES
+('A320', 'Airbus', 'A320-200', 1998, 73500.00, 'Commercial', 1, 'Jetliners', 'Short-haul narrow-body airliner'),
+('B737', 'Boeing', '737-800', 2005, 79015.00, 'Commercial', 2, 'Jetliners', 'Popular medium-range airliner'),
+('CRJ900', 'Bombardier', 'CRJ900', 2003, 37000.00, 'Regional', 3, 'Regional', NULL);
+
+/* --- Insert: Organization --- */
+INSERT INTO Organization (name, type, country, founding_year,  logo_url) VALUES
+('NASA', 'other', 'USA', 1958, NULL),
+('Boeing', 'other', 'USA', 1916, NULL),
+('Airbus', 'other', 'France', 1970, NULL);
+
+
+/* ---------------------------------------------------------- */
+/* ---------------------------------------------------------- */
+/* -------------------------- END --------------------------- */
+/* ---------------------------------------------------------- */
+/* ---------------------------------------------------------- */
